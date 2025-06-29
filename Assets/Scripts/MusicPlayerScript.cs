cf16424a6c20da29334642486522eb01eb0a9e26
LeTitouLapinou <162844593+LeTitouLapinou@users.noreply.github.com>
2025-06-29 18:52:55 -0400

    Final push

--- Assets/Scripts/MusicPlayerScript.cs
+++ Assets/Scripts/MusicPlayerScript.cs
@@ -1,25 +1,28 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MusicPlayerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public AudioSource AudioSource;
+    public AudioMixer mixer;
 
     private float musicVolume = .2f;
 
     void Start()
     {
-        AudioSource.Play();
+        //AudioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        AudioSource.volume = musicVolume;
+        //AudioSource.volume = musicVolume;
     }
 
     public void UpdateVolume(float volume)
     {
-        musicVolume = volume;
+        float normalized = volume / 0.2f; 
+        float clamped = Mathf.Max(normalized, 0.0001f);
+        mixer.SetFloat("MasterVolume", Mathf.Log10(clamped) * 20f);
     }
 }
